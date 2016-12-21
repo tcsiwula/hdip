faab399940fa1b574b26bcf0afd983445db03f71
tcsiwula <tcsiwula@usfca.edu>
2016-12-21 04:13:14 -0800

    hello world

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hdip
+#
+#custom domain --
+1. https://help.github.com/articles/using-a-custom-domain-with-github-pages/
+2. https://help.github.com/articles/quick-start-setting-up-a-custom-domain/
--- wiki.go
+++ wiki.go
@@ -1,6 +1,12 @@
-// Tim Siwula
-//https://golang.org/doc/articles/wiki/
-// https://github.com/tcsiwula/hdip
+//  Tim Siwula
+//  https://golang.org/doc/articles/wiki/
+//  https://github.com/tcsiwula/hdip
+//  https://pages.github.com/
+//
+// compile and run the program
+// go build wiki.go
+// ./wiki
+
 
 package main
 
@@ -9,14 +15,28 @@ import (
 	"io/ioutil"
 )
 
-
 type Page struct {
-    Title string
-    Body  []byte
+	Title string
+	Body  []byte
 }
 
-
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
