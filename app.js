b8f34af0d48dad1a90642dad345d4258f5b07222
Tim Siwula <tcsiwula@usfca.edu>
2016-12-23 10:20:47 -0600

    first react component

---
+++ app.js
@@ -0,0 +1,9 @@
+class Channel extends Teact.Component{
+    render() {
+        return(
+            <li> Channel Name</li>
+        )
+    }
+}
+
+ReactDOM.render(<Channel />, document.getElementById('app'));
