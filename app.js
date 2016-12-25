c1661fe9ec83df7d9ea84cb9892ceecb84456a21
Tim Siwula <tcsiwula@usfca.edu>
2016-12-25 16:45:01 -0600

    working react component

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-class Channel extends Teact.Component{
+class Channel extends React.Component {
+    onClick() {
+        console.log('Click ', this.props.name);
+    }
+
     render() {
-        return(
-            <li> Channel Name</li>
+        return (
+            <li onClick={this.onCLick.bind(this)}>
+                {this.props.name}</li>
         )
     }
 }
 
-ReactDOM.render(<Channel />, document.getElementById('app'));
+
+class Channel extends React.Component {
+    onClick() {
+        console.log('Click ', this.props.name);
+    }
+
+    render() {
+        return (
+            <ul>
+                <Channel name='Hardware Support'/>
+            </ul>
+        )
+    }
+}
+
+ReactDOM.render(<ChannelList />,
+    document.getElementById('app'));
